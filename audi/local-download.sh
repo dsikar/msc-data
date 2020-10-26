0f043349e76cfd07ee27ea19a6be250da05efc6e
Daniel Sikar <dsikar@gmail.com>
2020-10-26 12:39:42 +0000

    Added local scripts for Audi data downloads

---
+++ audi/local-download.sh
@@ -0,0 +1,24 @@
+# https://www.a2d2.audi/a2d2/en/download.html
+
+# Dataset License
+# Creative Commons Attribution-NoDerivatives 4.0 International License
+
+# This script downloads files locally i.e. not cloud environment, without batching
+
+# Gaimersheim
+# Bus Signals
+wget https://aev-autonomous-driving-dataset.s3.eu-central-1.amazonaws.com/camera_lidar-20180810150607_bus_signals.tar;
+# Camera – Front Center
+wget https://aev-autonomous-driving-dataset.s3.eu-central-1.amazonaws.com/camera_lidar-20180810150607_camera_frontcenter.tar;
+
+# Ingolstadt
+# Bus Signals
+wget https://aev-autonomous-driving-dataset.s3.eu-central-1.amazonaws.com/camera_lidar-20190401145936_bus_signals.tar;
+# Camera – Front Center
+wget https://aev-autonomous-driving-dataset.s3.eu-central-1.amazonaws.com/camera_lidar-20190401145936_camera_frontcenter.tar;
+
+# Munich
+# Bus Signals
+wget https://aev-autonomous-driving-dataset.s3.eu-central-1.amazonaws.com/camera_lidar-20190401121727_bus_signals.tar;
+# Camera – Front Center
+wget https://aev-autonomous-driving-dataset.s3.eu-central-1.amazonaws.com/camera_lidar-20190401121727_camera_frontcenter.tar;
