773561355d489ff18c29451d0a90c54e8848f9a6
Daniel Sikar <dsikar@gmail.com>
2020-10-26 21:17:22 +0000

    Template tidy up

--- udacity/devcloud-model.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# Notes to follow once we get onto intel devcloud
-# This is a placeholder.
-# DevCloud also has a queueing system. One difference
-# with downloads is full download path must be given to 
-# wget 
