b2d45b3a47a99605665946d04aeab9c2cf037cc6
Daniel Sikar <dsikar@gmail.com>
2020-10-26 21:34:01 +0000

    Added udacity direct download links

---
+++ udacity/udacity-download.sh
@@ -0,0 +1,7 @@
+# https://github.com/udacity/self-driving-car/tree/master/datasets
+
+# There are a total of 9 datasets available, all from Torrents. 
+# Additional 2 legacy datasets have direct download links.
+
+wget http://bit.ly/udacity-dataset-2-1;
+wget http://bit.ly/udacity-dataset-2-2;
